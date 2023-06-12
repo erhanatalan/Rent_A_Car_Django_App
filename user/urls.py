e4c4f8fe6dbca9bcb6b36525eccd91a841c40e5b
erhanatalan <erhanatalan2013@gmail.com>
2023-06-12 20:50:34 +0300

    user-urls

--- user/urls.py
+++ user/urls.py
@@ -4,3 +4,12 @@ from django.urls import path, include
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
 ]
+
+
+# routers
+from rest_framework.routers import DefaultRouter
+from .views import UserView
+router = DefaultRouter()
+router.register('', UserView)
+urlpatterns += router.urls
+
