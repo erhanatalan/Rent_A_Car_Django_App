3a856ab8045d62bfd45eb2c99563e4a01a4d5998
erhanatalan <erhanatalan2013@gmail.com>
2023-06-12 23:02:32 +0300

    car

--- car/urls.py
+++ car/urls.py
@@ -4,3 +4,13 @@ from django.urls import path, include
 urlpatterns = [
     
 ]
+
+
+# routers
+from rest_framework.routers import DefaultRouter
+from .views import CarView, ReservationView
+
+router = DefaultRouter()
+router.register('car', CarView)
+router.register('reservation', ReservationView)
+urlpatterns += router.urls
